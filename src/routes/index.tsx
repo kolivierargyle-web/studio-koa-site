9db94c8fb4de2b7e7aa9fad669208a33a8b8d974
kolivierargyle-web <kolivierargyle@gmail.com>
2026-09-04 21:33:47 +0100

    Make grid gaps black

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -188,7 +188,7 @@ function Index() {
           >
             Koa studio is a full-service creative direction
             <br />
-            &amp; production studio.Concept to final cut.
+            &amp; production studio. Concept to final cut.
             <br />
             Photography, video and brand identity.
             <br />
