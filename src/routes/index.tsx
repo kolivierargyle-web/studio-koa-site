4fe0bb9f2ff0c6ee8d6b1b586f13035131a36fee
kolivierargyle-web <kolivierargyle@gmail.com>
2026-09-05 09:58:39 +0100

    Make grid gaps black

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -183,7 +183,7 @@ function Index() {
             style={{
               fontFamily: "Poppins, sans-serif",
               fontSize: "clamp(1.25rem, 3.5vw, 40px)",
-              fontWeight: 300,
+              fontWeight: 200,
               lineHeight: "1.02",
               fontStyle: "italic",
             }}
