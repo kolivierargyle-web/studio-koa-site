dbb5c099c72f2a7264d134d519f7566bfd6d0ebb
kolivierargyle-web <kolivierargyle@gmail.com>
2026-09-08 16:27:40 +0100

    Reduce About section padding by 15%

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -199,7 +199,7 @@ function Index() {
   </div>
 </section>
 {/* Black banner divider */}
-<div className="w-full bg-black h-[0.9rem]"></div>
+<div className="w-full bg-black h-[0.86rem]"></div>
 
       {/* Work grid */}
       <section id="work" aria-label="Selected work">
