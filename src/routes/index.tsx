810acccbec71017fbc569aefd0ab2775947c392b
kolivierargyle-web <kolivierargyle@gmail.com>
2026-09-17 20:44:15 +0200

    Increase intro container width for 70px font

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -158,7 +158,7 @@ function Index() {
 
 {/* Intro */}
 <section id="about" className="px-[clamp(0.44rem,1.5vw,20px)] py-[clamp(3.3rem,9.75vw,190px)]">
-  <div className="mx-auto flex min-h-[199px] max-w-[1400px] items-center justify-center">
+  <div className="mx-auto flex min-h-[199px] max-w-[1600px] items-center justify-center">
     <div className="text-center">
       <div style={{
         fontFamily: "Besley, serif",
