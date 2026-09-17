70933f99fb94e243bf7946fd0c4576922e4ee872
kolivierargyle-web <kolivierargyle@gmail.com>
2026-09-17 21:25:04 +0200

    Limit nav bar scroll distance to stop at top of frame

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -85,7 +85,7 @@ function Index() {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
-  const navWorkTransform = Math.min(Math.max(scrollY - 800, 0), 1200);
+const navWorkTransform = Math.min(Math.max(scrollY - 800, 0), 700);
 
   return (
     <main className="bg-paper">
