e0940354033f8e877515d1d60b72615ddd742187
kolivierargyle-web <kolivierargyle@gmail.com>
2026-09-17 21:36:09 +0200

    Remove white space between grid and footer

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -85,7 +85,7 @@ function Index() {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
-const navWorkTransform = Math.min(Math.max(scrollY - 1400, 0), 450);
+const navWorkTransform = Math.min(Math.max(scrollY - 1400, 0), 350);
 
   return (
     <main className="bg-paper">
