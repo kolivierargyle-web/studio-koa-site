d54a8edb21730ba609349f8bcc8cae5c06834496
kolivierargyle-web <kolivierargyle@gmail.com>
2026-09-17 21:28:14 +0200

    Adjust nav scroll distance

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -85,7 +85,7 @@ function Index() {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
-const navWorkTransform = Math.min(Math.max(scrollY - 800, 0), 600);
+const navWorkTransform = Math.min(Math.max(scrollY - 800, 0), 650);
 
   return (
     <main className="bg-paper">
