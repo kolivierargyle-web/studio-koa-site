503531971dc851932e105e7fa7d7845dd2d40c07
kolivierargyle-web <kolivierargyle@gmail.com>
2026-09-17 15:10:11 +0200

    Set trigger point to 1500

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -141,11 +141,6 @@ useEffect(() => {
   window.addEventListener('scroll', handleScroll);
   return () => window.removeEventListener('scroll', handleScroll);
 }, []);
-
-  window.addEventListener('scroll', handleScroll);
-  return () => window.removeEventListener('scroll', handleScroll);
-}, []);
-
 const handleNavClick = (e, section) => {
   e.preventDefault();
   setActiveNav(section);
