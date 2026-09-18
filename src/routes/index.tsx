ee9928a6f77f93fa29cfaa1e75f07089ed2aea7f
kolivierargyle-web <kolivierargyle@gmail.com>
2026-09-18 13:05:25 +0200

    Make only first line italic, rest regular

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -239,13 +239,12 @@ function Index() {
               lineHeight: "53.487px",
               textAlign: "center",
               fontWeight: 400,
-              fontStyle: "italic",
               color: "rgba(0, 0, 0, 0.90)",
               maxWidth: "90%",
               margin: "0",
             }}>
               <p style={{ margin: 0 }}>
-                Koa studio is a compact full-service<br />
+                <span style={{ fontStyle: "italic" }}>Koa studio is a compact full-service</span><br />
                 creative direction & production studio.<br />
                 Photography, video, motion & identity.<br />
                 We build the right team for every project,<br />
