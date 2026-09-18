394bbf65ba5ee7bfb1ab43a21a12ffa09c7b1f04
kolivierargyle-web <kolivierargyle@gmail.com>
2026-09-18 17:03:29 +0200

    Center plus button vertically in white space

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -137,7 +137,7 @@ function Index() {
   style={{
     position: 'absolute',
     top: '09%',
-    right: '8px',
+    right: '7px',
     transform: 'translateY(-50%)',
     background: 'none',
     border: 'none',
@@ -153,8 +153,8 @@ function Index() {
     viewBox="0 0 144 144" 
     xmlns="http://www.w3.org/2000/svg"
   >
-    <line x1="72" y1="30" x2="72" y2="114" stroke="#000" strokeWidth="4" strokeLinecap="round"/>
-    <line x1="30" y1="72" x2="114" y2="72" stroke="#000" strokeWidth="4" strokeLinecap="round"/>
+    <line x1="72" y1="30" x2="72" y2="114" stroke="#000" strokeWidth="3.2" strokeLinecap="round"/>
+    <line x1="30" y1="72" x2="114" y2="72" stroke="#000" strokeWidth="3.2" strokeLinecap="round"/>
   </svg>
 </button>
 
