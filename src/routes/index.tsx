dc54ea70cfe893bce59167ee6d2679aeae0fbcc7
kolivierargyle-web <kolivierargyle@gmail.com>
2026-09-18 16:22:46 +0200

    Change background to white

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -132,12 +132,11 @@ function Index() {
       {/* Hero */}
       <section className="relative w-full overflow-hidden bg-ink flex-1 flex flex-col">
       <div className="relative w-full h-full">
-   {/* Frosted background behind video */}
+{/* White background behind video */}
 <div 
   className="absolute inset-0"
   style={{
-    backgroundColor: "rgba(255, 255, 255, 0.15)",
-    backdropFilter: "blur(14px)",
+    backgroundColor: "rgba(255, 255, 255, 1)",
   }}
 />
          <iframe
