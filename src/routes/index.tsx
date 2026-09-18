83b7f5566e71480612da046383a5cfcb96667032
kolivierargyle-web <kolivierargyle@gmail.com>
2026-09-18 16:55:26 +0200

    Center plus button vertically in white space

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -136,8 +136,8 @@ function Index() {
   onClick={() => document.getElementById('work').scrollIntoView({ behavior: 'smooth' })}
   style={{
     position: 'absolute',
-    top: '50%',
-    right: '40px',
+    top: '30%',
+    right: '35px',
     transform: 'translateY(-50%)',
     background: 'none',
     border: 'none',
