0a07207eaa410fc341f8cb00d3d803fb4c2965de
kolivierargyle-web <kolivierargyle@gmail.com>
2026-09-18 11:09:04 +0200

    Update footer with Didact Gothic and new dimensions

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -216,27 +216,11 @@ style={{ fontFamily: "Didact Gothic, sans-serif", fontWeight: 400, paddingLeft: 
           ))}
         </div>
       </section>
-
       {/* Footer */}
       <footer id="contact" className="bg-ink text-paper">
-        <div className="px-6 pb-[27px] pt-[177px] text-center">
-          <p
-            className="font-display text-paper"
-            style={{
-              fontFamily: "Poppins, sans-serif",
-              fontSize: "40px",
-              fontWeight: 700,
-              lineHeight: "normal",
-            }}
-          >
-            Koa Studio
-          </p>
-        </div>
-        <div>
-          <div
-            className="flex items-end justify-between px-[40px] py-11"
-          >
-            <a
+        <div style={{ width: "1443px", height: "276px" }}>
+          <div className="flex items-end justify-between px-[40px] py-11">
+            
               href="https://www.instagram.com/_koa_studio/"
               target="_blank"
               rel="noreferrer"
@@ -245,14 +229,15 @@ style={{ fontFamily: "Didact Gothic, sans-serif", fontWeight: 400, paddingLeft: 
             >
               <Instagram className="h-[34px] w-[34px]" strokeWidth={1.5} />
             </a>
-            <a
+            
               href="mailto:kat@studio-koa.com"
               className="transition-opacity hover:opacity-60"
               style={{
-                fontFamily: "Poppins, sans-serif",
-                fontSize: "18px",
-                fontWeight: 500,
+                fontFamily: "Didact Gothic, sans-serif",
+                fontSize: "20px",
+                fontWeight: 400,
                 lineHeight: "normal",
+                color: "#FFF",
               }}
             >
               kat@studio-koa.com
@@ -261,10 +246,11 @@ style={{ fontFamily: "Didact Gothic, sans-serif", fontWeight: 400, paddingLeft: 
           <div
             className="flex items-center justify-between border-t-2 border-paper px-[45px] py-11 uppercase"
             style={{
-              fontFamily: "Poppins, sans-serif",
+              fontFamily: "Didact Gothic, sans-serif",
               fontSize: "20px",
               fontWeight: 400,
               lineHeight: "normal",
+              color: "#FFF",
             }}
           >
             <span>London</span>
