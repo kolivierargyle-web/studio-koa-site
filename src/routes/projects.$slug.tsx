92af6fb741c77f2d42280097ab528b92908f6161
kolivierargyle-web <kolivierargyle@gmail.com>
2026-09-20 15:15:57 +0200

    Move credits image up

--- src/routes/projects.$slug.tsx
+++ src/routes/projects.$slug.tsx
@@ -175,7 +175,7 @@ function ProjectPage() {
             </div>
         </section>
         )}
-<section className="bg-paper py-16 text-center sm:py-20" style={{ marginTop: "100px", width: "100%" }}>
+<section className="bg-paper py-16 text-center sm:py-20" style={{ marginTop: "80px", width: "100%" }}>
   <img
     src={creditTextShyla}
     alt="Credits"
